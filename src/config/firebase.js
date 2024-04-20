b22e766196d7f64030a8198025af1572eecf2afa
Shane Khalid Lim <78730-22216550@SERVER2.DOMAIN>
2024-04-20 12:08:59 +0800

    commited

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGhuKC9ssjQmwpO0kiELMhvifeGjmFXG4",
@@ -18,3 +21,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+export const storage = getStorage(app);
